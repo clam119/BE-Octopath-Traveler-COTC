8e8b441de26bf4e6a05281c68a5004a106602157
clam119 <christopher.lam119@gmail.com>
2022-09-27 19:35:59 +0100

    Added the GET request for characters whose affiliation is power. Got rid of unused console logs and fixed an error where I placed the catch methods inside the then blocks.

--- server.js
+++ server.js
@@ -36,11 +36,11 @@ const server = http.createServer((req, res) => {
                 fs.writeFile(`${__dirname}/data/wealthCharacters.json`, JSON.stringify(wealthCharacters), (err) => {
                     if (err) console.log(err)
                 })
-            .catch((err) => {
-                if(err) console.log(err);
-            })
                 res.write(body)
                 res.end();
+            })
+            .catch((err) => {
+                if(err) console.log(err);
             })
     }
 
@@ -55,20 +55,37 @@ const server = http.createServer((req, res) => {
                 const parsedData = JSON.parse(data);
                 const fameCharacters = parsedData.filter(char => char.affiliation === 'Fame');
                 body += JSON.stringify(fameCharacters);
-                console.log(fameCharacters)
                 fs.writeFile(`${__dirname}/data/fameCharacters.json`, JSON.stringify(fameCharacters), (err) => {
                     if (err) console.log(err)
                 })
-            .catch((err) => {
-                if(err) console.log(err)
-            })
                 res.write(body)
                 res.end();
             })
+            .catch((err) => {
+                if(err) console.log(err)
+            })
     }
 
-
-
+    // 4 - GET Request - Return By Affiliation: Power
+    if(req.url === '/affiliation/power' && req.method === 'GET') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        let body = '';
+        fsPromise.readFile(`${__dirname}/data/traveler-index.json`)
+            .then((data) => {
+                const parsedData = JSON.parse(data);
+                const powerCharacters = parsedData.filter(char => char.affiliation === 'Power');
+                body += JSON.stringify(powerCharacters);
+                fs.writeFile(`${__dirname}/data/powerCharacters.json`, JSON.stringify(powerCharacters), (err) => {
+                    if (err) console.log(err)
+                })
+                res.write(body)
+                res.end();
+            })
+            .catch((err) => {
+                if(err) console.log(err)
+            })
+    }
 
 })
 
